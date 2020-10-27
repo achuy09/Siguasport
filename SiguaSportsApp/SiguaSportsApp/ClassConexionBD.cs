c9f2868f93e960dc340f24205a7a04d219d4845a
ange <angelchuy153@gmail.com>
2020-10-26 22:24:25 -0600

    Revert "Merge remote-tracking branch 'origin/master' into Angel"
    This reverts commit b14a9ab2f0a4bfebd5f9862b7c1989f54ba0d876.

--- SiguaSportsApp/SiguaSportsApp/ClassConexionBD.cs
+++ SiguaSportsApp/SiguaSportsApp/ClassConexionBD.cs
@@ -69,20 +69,19 @@ namespace SiguaSportsApp
                     verificacion = true;
                     MessageBox.Show("Bienvenido:  " + Nombre + "! Codigo de Puesto " + codigoPuesto, "Login Sunshine Network",
                         MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    CerrarConexion();
                 }
                 else
                 {
                     MessageBox.Show("Usuario/Contraseña incorrectos", "Ingreso", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    CerrarConexion();
                 }
             }
             catch(Exception e)
             {
                 MessageBox.Show("" + e);
-                CerrarConexion();
-            }                     
+            }
             
+            
+            CerrarConexion();
             return verificacion;
         }
     }
